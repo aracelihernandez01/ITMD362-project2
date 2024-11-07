cbc581275c5837bafc52249d63a1aee07df67c37
JinmingHuang <jhuang96@hawk.iit.edu>
2024-11-06 23:21:59 -0600

    basic

---
+++ CreditcardsPage/cardpage.js
@@ -0,0 +1,5 @@
+document.getElementById('sort').addEventListener('change', function() {
+  // Example of handling sorting
+  alert('Sorting by ' + this.value);
+  // Implement sorting functionality here
+});
