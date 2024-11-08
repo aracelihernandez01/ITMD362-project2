5802d1b709498b47d355f33c482fda1d49066097
aracelihernandez01 <ahernandez60@hawk.iit.edu>
2024-11-07 22:24:56 -0600

    script added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    alert("Redirecting to login page...");
+});
+window.addEventListener('scroll', function() {
+    const mainSection = document.querySelector('.main');
+    const scrollPosition = window.scrollY;
+    mainSection.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+});
